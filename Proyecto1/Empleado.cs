4cf4ab76ac292de62481415e8a16a4cb13d7f3cb
Mauricio Dottore <mauriciodottore@gmail.com>
2022-05-03 19:02:28 -0300

    Commit inicial de Proyecto 1

---
+++ Proyecto1/Empleado.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejercicio1
+{
+    class Empleado
+    {
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public int Edad { get; set; }
+
+        public Empleado(string nombre, string apellido, int edad)
+        {
+            Nombre = nombre;
+            Apellido = apellido;
+            Edad = edad;
+        }
+
+        public void ShowName()
+        {
+            Console.WriteLine("");
+        }
+    }
+}
---
+++ Proyecto1/Empresa.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejercicio1
+{
+    class Empresa
+    {
+
+        private static readonly Empresa instance = new Empresa();
+        private List<Empleado> Empleados = new List<Empleado>();
+
+        private Empresa() { }
+
+        public static Empresa getEmpresa()
+        {
+            return instance;
+        }
+
+    }
+}
